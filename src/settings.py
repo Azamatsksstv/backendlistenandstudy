59b0c32939bdf331e65934f77f70c119220738ee
Azamat <azambylbaev@gmail.com>
2023-08-03 00:31:49 +0300

    allowed hosts

--- src/settings.py
+++ src/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.89.158.96']
 AUTH_USER_MODEL = 'accounts.User'
 
 
